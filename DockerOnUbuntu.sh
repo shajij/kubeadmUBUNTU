2515843d0205fb7dfcc23d16d1e333d792af2f54
shajij <39598995+shajij@users.noreply.github.com>
2020-01-13 12:53:51 -0500

    Updated code to change the cgroup driver to system

--- DockerOnUbuntu.sh
+++ DockerOnUbuntu.sh
@@ -20,3 +20,22 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+systemctl enable docker
